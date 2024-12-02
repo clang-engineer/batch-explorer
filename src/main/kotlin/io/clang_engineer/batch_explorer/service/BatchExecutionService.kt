e5b8eb17dc0946906bfe86d7fe01f6fe5de5e139
clang-engineer <clang.engineer@gmail.com>
2024-12-02 09:52:56 +0900

    feat: BatchResource - stop, restart api

--- src/main/kotlin/io/clang_engineer/batch_explorer/service/BatchExecutionService.kt
+++ src/main/kotlin/io/clang_engineer/batch_explorer/service/BatchExecutionService.kt
@@ -2,12 +2,15 @@ package io.clang_engineer.batch_explorer.service
 
 import io.clang_engineer.batch_explorer.service.dto.JobExecutionDTO
 import io.clang_engineer.batch_explorer.service.dto.StepExecutionDTO
+import org.springframework.batch.core.BatchStatus
 import org.springframework.batch.core.explore.JobExplorer
+import org.springframework.batch.core.launch.JobOperator
 import org.springframework.stereotype.Service
 
 @Service
-class BatchMetaService(
+class BatchExecutionService(
   private val jobExplorer: JobExplorer,
+  private val jobOperator: JobOperator
 ) {
   private val log = org.slf4j.LoggerFactory.getLogger(javaClass)
 
@@ -61,6 +64,31 @@ class BatchMetaService(
       }
     }
     return result
+  }
+
+  fun stopJobExecution(jobExecutionId: Long) {
+    log.info("Stopping job execution: $jobExecutionId")
+
+    val jobExecution = jobExplorer.getJobExecution(jobExecutionId) as org.springframework.batch.core.JobExecution
+    if (jobExecution !== null && jobExecution.isRunning) {
+      jobOperator.stop(jobExecutionId)
+    }
+  }
+
+  fun restartJobExecution(jobExecutionId: Long) {
+    log.info("Restarting job execution: $jobExecutionId")
+
+    val jobExecution = jobExplorer.getJobExecution(jobExecutionId)
+    if (jobExecution?.status == BatchStatus.FAILED) {
+      try {
+        jobOperator.restart(jobExecutionId)
+        log.info("JobExecution $jobExecutionId has been restarted.")
+      } catch (e: Exception) {
+        log.error("Failed to restart job execution: $jobExecutionId", e)
+      }
+    } else {
+      log.info("JobExecution $jobExecutionId is not in a failed state.")
+    }
   }
 }
 
--- src/main/kotlin/io/clang_engineer/batch_explorer/web/BatchResource.kt
+++ src/main/kotlin/io/clang_engineer/batch_explorer/web/BatchResource.kt
@@ -1,40 +1,38 @@
 package io.clang_engineer.batch_explorer.web
 
-import io.clang_engineer.batch_explorer.service.BatchMetaService
+import io.clang_engineer.batch_explorer.service.BatchExecutionService
 import io.clang_engineer.batch_explorer.service.dto.JobExecutionDTO
-import org.springframework.batch.core.JobInstance
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class BatchResource(
-  private val batchMetaService: BatchMetaService
+  private val batchExecutionService: BatchExecutionService
 ) {
   private val log = org.slf4j.LoggerFactory.getLogger(javaClass)
-
-  @GetMapping("/job/instances")
-  fun fetchAllJobs(): ResponseEntity<List<JobInstance>> {
-    log.info("Listing all jobs")
-
-//    val jobInstances = jobExplorer.findJobInstancesByJobName("batch-explorer-job", 0, 100)
-    return ResponseEntity.ok().build()
-  }
 
   @GetMapping("/job/executions")
   fun fetchAllJobExecutions(): ResponseEntity<List<JobExecutionDTO>> {
     log.info("Listing all job executions")
 
-    val jobExecutions = batchMetaService.fetchAllExecutions()
+    val jobExecutions = batchExecutionService.fetchAllExecutions()
     return ResponseEntity.ok(jobExecutions)
   }
 
-  @GetMapping("/job/steps")
-  fun fetchAllJobSteps(): ResponseEntity<List<org.springframework.batch.core.StepExecution>> {
-    log.info("Listing all job steps")
+  @PostMapping("/job/executions/{executionId}/stop")
+  fun stopExecution(@PathVariable executionId: Long): ResponseEntity<Void> {
+    log.info("Stopping job execution: $executionId")
 
+    batchExecutionService.stopJobExecution(executionId)
+    return ResponseEntity.ok().build()
+  }
+
+  @PostMapping("/job/executions/{executionId}/restart")
+  fun restartExecution(@PathVariable executionId: Long): ResponseEntity<Void> {
+    log.info("Restarting job execution: $executionId")
+
+    batchExecutionService.restartJobExecution(executionId)
     return ResponseEntity.ok().build()
   }
 
