8410637951f043b45bd99963f6057e60b303291f
clang-engineer <clang.engineer@gmail.com>
2024-12-07 16:44:26 +0900

    refactor: make batch job as spring context bean

--- src/main/kotlin/io/clang_engineer/batch_explorer/config/BatchConfiguration.kt
+++ src/main/kotlin/io/clang_engineer/batch_explorer/config/BatchConfiguration.kt
@@ -1,8 +1,48 @@
 package io.clang_engineer.batch_explorer.config
 
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
 @EnableBatchProcessing
-class BatchConfiguration
+class BatchConfiguration(
+        private val transactionManager: PlatformTransactionManager, private val jobRepository: JobRepository,
+) {
+    private val JOB_NAME = "batch-explorer-job"
+    private val STEP_NAME = "batch-explorer-step"
+
+    @Bean
+    fun sampleJob(): Job {
+        return JobBuilder(JOB_NAME, jobRepository).start(sampleStep()).build()
+    }
+
+    @Bean
+    fun sampleStep(): Step {
+        return StepBuilder(STEP_NAME, jobRepository)
+                .tasklet(sampleTasklet(), transactionManager).build()
+    }
+
+    @Bean
+    fun sampleTasklet(): Tasklet {
+        return Tasklet { _, _ ->
+            synchronized(this) {
+                val file = java.io.File("build/output.txt")
+
+                if (!file.exists()) {
+                    file.createNewFile()
+                }
+
+                file.appendText("Job executed at ${java.time.LocalDateTime.now()}\n")
+            }
+            null
+        }
+    }
+}
--- src/main/kotlin/io/clang_engineer/batch_explorer/job/QuartzJob.kt
+++ src/main/kotlin/io/clang_engineer/batch_explorer/job/QuartzJob.kt
@@ -2,65 +2,18 @@ package io.clang_engineer.batch_explorer.job
 
 import org.quartz.JobExecutionContext
 import org.springframework.batch.core.Job
-import org.springframework.batch.core.Step
-import org.springframework.batch.core.configuration.JobRegistry
-import org.springframework.batch.core.configuration.support.ReferenceJobFactory
-import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.batch.core.repository.JobRepository
-import org.springframework.batch.core.step.builder.StepBuilder
-import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.stereotype.Component
-import org.springframework.transaction.PlatformTransactionManager
 
 @Component
 class QuartzJob(
         private val jobLauncher: JobLauncher,
-        private val jobRepository: JobRepository,
-        private val transactionManager: PlatformTransactionManager,
-        private val jobRegistry: JobRegistry,
+        private val sampleJob: Job
 ) : org.quartz.Job {
-    private val JOB_NAME = "batch-explorer-job"
-    private val STEP_NAME = "batch-explorer-step"
-
     override fun execute(context: JobExecutionContext?) {
         val jobDataMap = context?.jobDetail?.jobDataMap as org.quartz.JobDataMap
         val jobParameters = transformQuartzJobDataMapToBatchJobParameters(jobDataMap)
 
-        var job: Job? = try {
-            jobRegistry.getJob(JOB_NAME)
-        } catch (e: org.springframework.batch.core.launch.NoSuchJobException) {
-            null
-        }
-
-        if (job == null) {
-            job = job()
-            jobRegistry.register(ReferenceJobFactory(job))
-        }
-
-        jobLauncher.run(job(), jobParameters)
-    }
-
-    fun job(): Job {
-        return JobBuilder(JOB_NAME, jobRepository).start(step()).build()
-    }
-
-    fun step(): Step {
-        return StepBuilder(STEP_NAME, jobRepository).tasklet(tasklet(), transactionManager).build()
-    }
-
-    fun tasklet(): Tasklet {
-        return Tasklet { _, _ ->
-            synchronized(this) {
-                val file = java.io.File("build/output.txt")
-
-                if (!file.exists()) {
-                    file.createNewFile()
-                }
-
-                file.appendText("Job executed at ${java.time.LocalDateTime.now()}\n")
-            }
-            null
-        }
+        jobLauncher.run(sampleJob, jobParameters)
     }
 }
