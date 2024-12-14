a327d86f9cb8be7d2952b918585a3b58a3ed5679
clang-engineer <clang.engineer@gmail.com>
2024-12-14 12:14:38 +0900

    chore

--- src/main/kotlin/io/clang_engineer/batch_explorer/job/JobUtils.kt
+++ src/main/kotlin/io/clang_engineer/batch_explorer/job/JobUtils.kt
@@ -16,6 +16,8 @@ fun transformQuartzJobDataMapToBatchJobParameters(jobDataMap: org.quartz.JobData
     }
 
     // Add timestamp to execute unique batch job
+    // quartz를 사용해서 주기적으로 같은 batch job instance를 실행하기 위해 별도의 고유값을 추가한다.
+    // spring batch에서 job instance는 job name과 job parameters로 구분되기 때문에 job parameters에 timestamp를 추가한다.
     jobParametersBuilder.addLong("timestamp", System.currentTimeMillis())
 
     return jobParametersBuilder.toJobParameters()
